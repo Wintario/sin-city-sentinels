518d34a60c61649e03312b444855c470f0c42748
Wintario <wintario@example.com>
2026-03-14 00:21:08 +0300

    Hotfix film grain flicker by removing JS opacity jitter

--- src/components/FilmGrain.tsx
+++ src/components/FilmGrain.tsx
@@ -1,22 +1,5 @@
-import { useEffect, useState } from 'react';
-
 const FilmGrain = () => {
-  const [opacity, setOpacity] = useState(0.05);
-
-  useEffect(() => {
-    const flickerInterval = setInterval(() => {
-      setOpacity(0.03 + Math.random() * 0.04);
-    }, 100);
-
-    return () => clearInterval(flickerInterval);
-  }, []);
-
-  return (
-    <div
-      className="film-grain"
-      style={{ opacity }}
-    />
-  );
+  return <div className="film-grain" />;
 };
 
 export default FilmGrain;
